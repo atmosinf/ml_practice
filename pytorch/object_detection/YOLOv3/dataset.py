ac5d95d04d16cc46daf829e1389a7917d91f9f28
atmosinf <73055038+atmosinf@users.noreply.github.com>
2022-06-20 10:33:11 +0300

    yolov3 done

--- pytorch/object_detection/YOLOv3/dataset.py
+++ pytorch/object_detection/YOLOv3/dataset.py
@@ -28,12 +28,12 @@ class YOLODataset(Dataset):
         self.ignore_iou_thresh = 0.5
         self.image_size = image_size
 
-    def _len__(self):
+    def __len__(self):
         return len(self.annotations)
 
     def __getitem__(self, index):
         label_path = os.path.join(self.label_dir, self.annotations.iloc[index, 1])
-        bboxes = np.roll(np.loadtxt(fname=label_path, delimiter=' ', ndim=2), 4, axis=1).tolist() # to get the class in the first dimension (not sure why)
+        bboxes = np.roll(np.loadtxt(fname=label_path, delimiter=' ', ndmin=2), 4, axis=1).tolist() # to get the class in the first dimension (not sure why)
         img_path = os.path.join(self.img_dir, self.annotations.iloc[index, 0])
         image = np.array(Image.open(img_path).convert('RGB'))
 
@@ -42,8 +42,39 @@ class YOLODataset(Dataset):
             image = augmentations['image']
             bboxes = augmentations['bboxes']
 
-        targets = [torch.zeros((self.num_anchors // 3, S, S, 6)) for S in self.S] # 6 because [p_object, x, y, w, h, class]
+        targets = [torch.zeros((self.num_anchors // 3, S, S, 6)) for S in self.S] # 6 because [p_object, x, y, w, h, class]. num_anchors is 9 (total num of anchors)
 
         for box in bboxes:
-            
+            iou_anchors = iou(torch.tensor(box[2:4]), self.anchors) # [2:4] to get just the height and the width
+            anchor_indices = iou_anchors.argsort(descending=True, dim=0) # the first one is going to be the best anchor
+            x, y, height, width, class_label = box
+            has_anchor = [False, False, False]
+
+            for anchor_idx in anchor_indices: # to find which anchor the bbox belongs to
+                scale_idx = anchor_idx // self.num_anchors_per_scale # will be 0, 1 or 2. tells us which target to take out of the 'targets' list
+                anchor_on_scale = anchor_idx % self.num_anchors_per_scale # will be 0, 1 or 2. which anchor in that particular scale
+                S = self.S[scale_idx]
+                i, j = int(S*y), int(S*x) # which cell does the bbox belong to. x and y are fractions of the image
+                anchor_taken = targets[scale_idx][anchor_on_scale, i, j, 0]
+
+                if not anchor_taken and not has_anchor[scale_idx]:
+                    targets[scale_idx][anchor_on_scale, i, j, 0] = 1
+                    x_cell, y_cell = x*S - j, y*S - i
+                    width_cell, height_cell = (
+                        width * S, 
+                        height * S,
+                    )
+                    box_coordinates = torch.tensor(
+                        [x_cell, y_cell, width_cell, height_cell]
+                    )
+                    targets[scale_idx][anchor_on_scale, i, j, 1:5] = box_coordinates
+                    targets[scale_idx][anchor_on_scale, i, j, 5] = int(class_label)
+                    has_anchor[scale_idx] = True
+
+                elif not anchor_taken and iou_anchors[anchor_idx] > self.ignore_iou_thresh:
+                    targets[scale_idx][anchor_on_scale, i, j, 0] = -1 # -1 means that we're going to ignore this prediction
+
+        return image, tuple(targets)
+
+
         
